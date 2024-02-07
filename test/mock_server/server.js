2aad419978395409df1acbbb30f8e3882801a4fe
Alek <ole.alexander89@gmail.com>
2024-02-07 17:19:27 +0100

    tests woring

--- test/mock_server/server.js
+++ test/mock_server/server.js
@@ -3,37 +3,36 @@ const https = require('node:https');
 var fs = require('fs');
 const { assert } = require('node:console');
 
-
+const express = require("express")
+const app = express()
 const hostname = '0.0.0.0';
 
 const mockFn = (req, res) => {
-    res.statusCode = 200;
+
     console.log(req.url)
-    console.log(Object.keys(req))
+    console.log(req.get("host"))
     if (req.hostname.includes("my-ip")) {
         res.setHeader('Content-Type', 'text/plain');
-        res.end("127.0.0.1\nsgdfgssdfgsdg\nrfsdfsdfsdbs")
+        res.send("127.0.0.1\nsgdfgssdfgsdg\nrfsdfsdfsdbs")
     }
 
     else {
         assert(req.method === "PUT")
         assert(req.url === "/v1/domains/organiccode.net/records/A/%40", "URL does not match expected: " + req.url)
-        assert(req.headers["Authorization"] === "sso-key test token")
-
-        const chunks = [];
-        req.on('data', chunk => chunks.push(chunk));
-        req.on('end', () => {
-            console.log(chunks)
-            const data = JSON.parse(Buffer.concat(chunks));
-            assert(data.data === "127.0.0.1")
-        })
-        res.end();
+        assert(req.get("Authorization") === "sso-key test token")
+        assert(req.body.data === "127.0.0.1")
+        
+        res.send();
     }
 }
 
-const httpServer = http.createServer(mockFn);
+app.use(express.json());
+app.use((err, req, res, next) => {
+    console.log(err)
+})
+app.all(/\/.*/, mockFn);
 
-httpServer.listen(80, hostname, () => {
+http.createServer(app).listen(80, hostname, () => {
     console.log(`Server running at http://${hostname}:${80}/`);
 });
 
@@ -44,8 +43,6 @@ const options = {
 
 };
 
-const httpsServer = https.createServer(options, mockFn);
-
-httpsServer.listen(443, hostname, () => {
+https.createServer(options, app).listen(443, hostname, () => {
     console.log(`Server running at https://${hostname}:${443}/`);
 });
